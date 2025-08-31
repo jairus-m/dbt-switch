650d44d8c022ad9c0bfd65db380d76dae75a5754
Jairus Martinez <jairusmartinez@gmail.com>
2025-08-30 19:14:24 -1000

    Add basic config modifying commands: - Implement add, update, and delete -  Add input handling and logging

--- src/dbt_switch/utils/__init__.py
+++ src/dbt_switch/utils/__init__.py
@@ -1,3 +1,15 @@
+from .logger import logger
 from .config_handler import init_config
+from .input_handler import (
+    add_user_config_input,
+    update_user_config_input,
+    delete_user_config_input,
+)
 
-__all__ = ["init_config"]
+__all__ = [
+    "logger",
+    "init_config",
+    "add_user_config_input",
+    "update_user_config_input",
+    "delete_user_config_input",
+]
